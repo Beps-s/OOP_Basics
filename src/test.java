6e10cfc48da34a1505db7de86cdd2381801bed9f
Chris Erman <chris.erman@khk.ee>
2020-12-18 17:46:23 +0200

    Teade väljastamine Java abil

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Tere, Java programm");
-        System.out.println("See on Java IDE katse");
+        System.out.println("Täna õppisime");
+        System.out.println("1. JDK Installeerimine ja seadistamine");
+        System.out.println("2. IDE Installeerimine ja seadistamine");
+        System.out.println("2. IDE Kasutamine");
+        System.out.println("2. git VCS Kasutamine");
     }
 }
