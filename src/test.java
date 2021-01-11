d6f053fcbba32717422174b1d6efa1aa3951c5bb
Chris Erman <chris.erman@khk.ee>
2021-01-11 15:52:47 +0200

    Massiivid - ülesanne 5.1

--- src/test.java
+++ src/test.java
@@ -4,15 +4,37 @@
  * Massiivid
  * */
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        int[] vastuvoetud = {2803, 2626, 2359, 1927, 2236, 2281, 2394, 2484, 2468};
-        Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisestage aasta: ");
-        int aasta = sisend.nextInt();
-        int index = aasta - 2011;
-        System.out.println(aasta + ". aastal on vastuvõetud " + vastuvoetud[index]);
+        // massiiv faili sisu hoidmiseks
+        ArrayList<String> aastad = new ArrayList<>();
+        // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
+        File fail = new File("C:\\Users\\chris\\IdeaProjects\\oop_alused\\src\\vastuvoetud.txt");
+        Scanner sisendFailist = null;
+        try {
+            sisendFailist = new Scanner(fail);
+        } catch (Exception e) {
+            System.out.println("Faili pole - " + e.getMessage());
+        }
+        // loeme failist
+        while (sisendFailist.hasNextLine()) {
+            String rida = sisendFailist.nextLine();
+            aastad.add(rida); // lisame loetud väärtus nimekirja sisse
+        }
+        sisendFailist.close();
+        // vaatame nimekirja sisu
+        for (int i = 0; i < aastad.size(); i++) {
+            System.out.println(aastad.get(i));
+        }
+        // küsime andmed kasutaja käest
+        Scanner sisendKasutajalt = new Scanner(System.in);
+        System.out.println("Sisesta aasta: ");
+        int aasta = sisendKasutajalt.nextInt();
+        sisendKasutajalt.close();
+        System.out.println("2011 aastas vastu võetud " + aastad.get(aasta - 2011));
     }
 }
