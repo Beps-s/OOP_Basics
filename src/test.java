2bcb068f5d02ff80441abe95bd4ccddef3b0d86c
Chris Erman <chris.erman@khk.ee>
2021-01-13 13:36:48 +0200

    Massiivid - ülesanne 5.4c

--- src/test.java
+++ src/test.java
@@ -1,10 +1,13 @@
 /*
  * autor - Chris Erman
- * ülesanne 5.4a
+ * ülesanne 5.4c
  * Massiivid
  * */
 
+
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -24,8 +27,12 @@ public class test {
         }
         sisendFailist.close();
 
-        for (int i = 0; i < opilased.size(); i++) {
-            System.out.println(i opilased.get(i));
-        }
+        LocalDateTime tananeKuupaev = LocalDateTime.now();
+        System.out.println("Tänane kuupäev ilma vorminduseta " + tananeKuupaev);
+        DateTimeFormatter kuupaevaVormindus = DateTimeFormatter.ofPattern("dd");
+        String tananeKuupaevVormindatud = tananeKuupaev.format(kuupaevaVormindus);
+        System.out.println("Tänane vormindatud kuupäev " + tananeKuupaevVormindatud);
+        int indeks = Integer.parseInt(tananeKuupaevVormindatud);
+        System.out.println(opilased.get(indeks - 1));
     }
 }
