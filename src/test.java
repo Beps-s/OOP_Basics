7dea04c1b330f06d75a0d79e0f2c60280de18812
Chris Erman <chris.erman@khk.ee>
2021-01-13 13:40:20 +0200

    Massiivid - ülesanne 5.3

--- src/test.java
+++ src/test.java
@@ -1,20 +1,18 @@
 /*
  * autor - Chris Erman
- * ülesanne 5.4c
+ * ülesanne 5.3
  * Massiivid
  * */
 
 
 import java.io.File;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        ArrayList<String> opilased = new ArrayList<>();
-        File fail = new File("C:\\Users\\chris\\IdeaProjects\\oop_alused\\src\\nimekiri.txt");
+        ArrayList<Double> tehingud = new ArrayList<>();
+        File fail = new File("C:\\Users\\chris\\IdeaProjects\\oop_alused\\src\\konto.txt");
         Scanner sisendFailist = null;
         try {
             sisendFailist = new Scanner(fail);
@@ -22,17 +20,14 @@ public class test {
             System.out.println("Faili pole - " + e.getMessage());
         }
         while (sisendFailist.hasNextLine()) {
-            String rida = sisendFailist.nextLine();
-            opilased.add(rida);
+            Double rida = sisendFailist.nextDouble();
+            tehingud.add(rida);
         }
         sisendFailist.close();
-
-        LocalDateTime tananeKuupaev = LocalDateTime.now();
-        System.out.println("Tänane kuupäev ilma vorminduseta " + tananeKuupaev);
-        DateTimeFormatter kuupaevaVormindus = DateTimeFormatter.ofPattern("dd");
-        String tananeKuupaevVormindatud = tananeKuupaev.format(kuupaevaVormindus);
-        System.out.println("Tänane vormindatud kuupäev " + tananeKuupaevVormindatud);
-        int indeks = Integer.parseInt(tananeKuupaevVormindatud);
-        System.out.println(opilased.get(indeks - 1));
+        for (int i = 0; i < tehingud.size(); i++) {
+            if (tehingud.get(i) >= 0) {
+                System.out.println(tehingud.get(i));
+            }
+        }
     }
 }
