56d82dc10820695a9fa089e45983c22703fab4bd
Chris Erman <chris.erman@khk.ee>
2021-01-15 12:39:46 +0200

    Masiivid - ülesanne 6.4a

--- src/test.java
+++ src/test.java
@@ -1,38 +1,31 @@
 /*
  * autor - Chris Erman
- * ülesanne 5.3
- * Massiivid
+ * ülesanne 6.4a
+ * meetod + return
  * */
 
-
-import java.io.File;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
+    static String tervitus(int kord) {
+        System.out.println("Võõrustaja: \"Tere!\"");
+        System.out.println("Täna " + kord + ". kord tervitada, mõtiskleb võõrustaja.");
+        System.out.println("Külaline: \"Tere, suur tänu kutse eest!\"");
+        return null;
+    }
+
     public static void main(String[] args) {
-        ArrayList<String> opilased = new ArrayList<>();
-        File fail = new File("C:\\Users\\chris\\IdeaProjects\\oop_alused\\src\\nimekiri.txt");
-        Scanner sisendFailist = null;
-        try {
-            sisendFailist = new Scanner(fail);
-        } catch (Exception e) {
-            System.out.println("Faili pole - " + e.getMessage());
+        // defineerime muutujad
+        // lubame kasutajal sisestada midagi
+        Scanner sisend = new Scanner(System.in);
+        // selgitame kasutajale, mida tuleb sisestada
+        System.out.println("Sisestage külaliste arv: ");
+        // salvestame kasutaja sisestatud väärtuse
+        int kord = sisend.nextInt();
+        int kd = 1;
+        while (kord >= kd) {
+            tervitus(kd);
+            kd++;
         }
-        while (sisendFailist.hasNextLine()) {
-            String rida = sisendFailist.nextLine();
-            opilased.add(rida);
-        }
-        sisendFailist.close();
-        LocalDateTime tananeKuupaev = LocalDateTime.now();
-        System.out.println("Tänane kuupäev ilma vorminduseta " + tananeKuupaev);
-        DateTimeFormatter kuupaevaVormindus = DateTimeFormatter.ofPattern("dd");
-        String tananeKuupaevVormindatud = tananeKuupaev.format(kuupaevaVormindus);
-        System.out.println("Tänane vormindatud kuupäev " + tananeKuupaevVormindatud);
-        // otsime vajalik inimene nimekirjast
-        int indeks = Integer.parseInt(tananeKuupaevVormindatud);
-        System.out.println(opilased.get(indeks - 1));
     }
 }
